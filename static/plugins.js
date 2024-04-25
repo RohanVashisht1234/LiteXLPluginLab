e1e0ed0aa345344feff02482bb1cb8840d62ab9e
rohanvashisht <rohanprogrammer1@gmail.com>
2024-04-25 14:59:00 +0530

    ok

--- static/plugins.js
+++ static/plugins.js
@@ -34,14 +34,14 @@ function buildHtml(data) {
 }
 
 function handle_typing() {
-    var parent_div = document.getElementById("place_cards_here");
-    parent_div.innerHTML = "";
+    var html = "";
+    var thing_that_is_being_typed_in_the_search_box = document.getElementById("searchbox").value;
+    if (thing_that_is_being_typed_in_the_search_box == "") {
+        var parent_div = document.getElementById("place_cards_here");
+        parent_div.innerHTML = globalData;
+    }
     var data = globalData;
-    for (var i = 0; i < data["addons"].length; i++) {
-        // The following is done to exclude the first 3 unnessecary entries.
-        if (i < 3) {
-            continue;
-        }
+    for (var i = 3; i < data["addons"].length; i++) {
         var name = data["addons"][i]['id'];
         name = name;
         var title = name;
@@ -49,21 +49,22 @@ function handle_typing() {
         title = title.charAt(0).toUpperCase() + title.slice(1);
         var description = data["addons"][i]['description'];
         description = description;
-        var thing_that_is_being_typed_in_the_search_box = document.getElementById("searchbox").value;
         if ("name" in data["addons"][i]) {
             title = data["addons"][i]['name'];
             title = title;
         }
         if (title.includes(thing_that_is_being_typed_in_the_search_box) || description.includes(thing_that_is_being_typed_in_the_search_box) || name.includes(thing_that_is_being_typed_in_the_search_box)) {
-            parent_div.innerHTML += `<div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">${title.replace(thing_that_is_being_typed_in_the_search_box, "<span style='background-color:yellow;color:black;'>" + thing_that_is_being_typed_in_the_search_box + "</span>")}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">${name.replace(thing_that_is_being_typed_in_the_search_box, "<span style='background-color:yellow;color:black;'>" + thing_that_is_being_typed_in_the_search_box + "</span>")}</h6>
-          <p class="card-text markdownContent">${marked.parse(description).replace(thing_that_is_being_typed_in_the_search_box, "<span style='background-color:yellow;color:black;'>" + thing_that_is_being_typed_in_the_search_box + "</span>")}</p>
-          <a href="/@plugins/plugin_slug?plugin=${name}" class="card-link btn btn-primary">View plugin</a>
-        </div>
-        </div>`;
+            html += `<div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">${title.replace(thing_that_is_being_typed_in_the_search_box, "<span style='background-color:yellow;color:black;'>" + thing_that_is_being_typed_in_the_search_box + "</span>")}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${name.replace(thing_that_is_being_typed_in_the_search_box, "<span style='background-color:yellow;color:black;'>" + thing_that_is_being_typed_in_the_search_box + "</span>")}</h6>
+              <p class="card-text markdownContent">${marked.parse(description).replace(thing_that_is_being_typed_in_the_search_box, "<span style='background-color:yellow;color:black;'>" + thing_that_is_being_typed_in_the_search_box + "</span>")}</p>
+              <a href="/@plugins/plugin_slug?plugin=${name}" class="card-link btn btn-primary">View plugin</a>
+            </div>
+            </div>`
         }
+        var parent_div = document.getElementById("place_cards_here");
+        parent_div.innerHTML = html;
     }
 }
 
